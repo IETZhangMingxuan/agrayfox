f5cc7905a36c2fb2ee9adc4e2bea6d6dfae091d0
Grayfox <1628029199@qq.com>
2021-06-27 11:23:44 +0800

    2.0版本优化了短视频页,接入了快手的部分接口.优化封装了热点总组件.添加了主页的自定义导航组件.优化了联系站长页.增添了登录注册页及实验室组件

--- src/pages/Login/Test.vue
+++ src/pages/Login/Test.vue
@@ -1,7 +1,14 @@
 <template>
   <div id="test">
-    <h4>测试组件</h4>
-    <h4>测试目的:封装热点总路由页,单独封装出一个组件供整个路由页使用</h4>
+    <h4>实验室组件</h4>
+    <h4>你居然发现了这里</h4>
+    <h4>这里是该网站的测试组件,用于对新功能的测试,如果有什么需求,可以在留言板写下你宝贵的想法</h4>
+    <h4>现在是2.0版本</h4>
+    <h4>接下来需要实现的需求有:</h4>
+    <h4>1.用户修改密码</h4>
+    <h4>2.用户修改头像</h4>
+    <h4>3.添加钢琴模块</h4>
+    <h4>4.优化新闻页,添加更多接口</h4>
   </div>
 </template>
 <script>
@@ -13,4 +20,15 @@ export default {
   methods: {}
 };
 </script>
-<style scoped lang="less"></style>
+<style scoped lang="less">
+#test{
+  margin: 50px auto;
+  h4{
+    font-family: "SF Pro Display", Arial, "PingFang SC", "Hiragino Sans GB","Microsoft YaHei", sans-serif;
+    padding: 10px auto;
+    margin-top: 10px;
+    font-size:20px;
+
+  }
+}
+</style>
